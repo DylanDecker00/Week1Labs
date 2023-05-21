581983c68c782d77d28426cd018e2361a2f1c3d6
DylanDecker00 <masterlock1212@gmail.com>
2023-05-20 23:38:55 -0500

    first commit

---
+++ src/com/promineotech/Application.java
@@ -0,0 +1,17 @@
+package com.promineotech;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("My First Project");
+		int age = 67;
+		System.out.println(age);
+		double accountBalance = 34.67;
+		System.out.println(accountBalance);
+		char middleIntital = 'C';
+		boolean isHotOutside = true;
+		String firstName = "sam";
+		System.out.println(firstName);
+	}
+
+}
